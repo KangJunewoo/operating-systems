0159059222b29b11bd5c44860419d63b37994d94
KangJunewoo <brianjune.dev@gmail.com>
2020-10-04 00:07:14 +0900

    [REFACTOR&ADD] 메세지큐 주석달기 + 실행 gif 추가

--- 4주차/msg_queue_reader.c
+++ 4주차/msg_queue_reader.c
@@ -10,21 +10,22 @@
 struct msg_buffer{
     long msg_type;
     char msg_text[100];
-} msg;
+} msg; // 메
 
 int main(){
+    // key와 메세지id 선언해서
     key_t key;
-    int msg_id;
+    int msg_id; 
 
-    key = ftok("progfile", 65);
+    // key랑 msg_id를 각 
+    key = ftok("progfile", 65); // ftok은 ipc키 생성함수
+    msg_id = msgget(key, 0666 | IPC_CREAT); // 위의 ipc키를 통해 msg를 생성하는 것 같고..
 
-    msg_id = msgget(key, 0666 | IPC_CREAT);
+    msgrcv(msg_id, &msg, sizeof(msg), 1, 0); // 큐에서 메세지를 받아서
 
-    msgrcv(msg_id, &msg, sizeof(msg), 1, 0);
+    printf("Data Received : %s\n", msg.msg_text); // 뿌리고
 
-    printf("Data Received : %s\n", msg.msg_text);
-
-    msgctl(msg_id, IPC_RMID, NULL);
+    msgctl(msg_id, IPC_RMID, NULL); // 메세지큐 삭제
 
     return 0;
 }
--- 4주차/msg_queue_writer.c
+++ 4주차/msg_queue_writer.c
@@ -16,16 +16,16 @@ int main(){
     key_t key;
     int msg_id;
 
+    // 메세지 키 & 아이디 & 타입 설정
     key = ftok("progfile", 65);
-
     msg_id = msgget(key, 0666 | IPC_CREAT);
     msg.msg_type = 1;
 
     printf("Write Data : ");
-    fgets(msg.msg_text, 100, stdin);
+    fgets(msg.msg_text, 100, stdin); // gets를 썼더니 경고가 떠서 fgets로 대체
     // gets(msg.msg_text);
 
-    msgsnd(msg_id, &msg, sizeof(msg), 0);
+    msgsnd(msg_id, &msg, sizeof(msg), 0); // 해당 메세지 전송
 
     printf("Data Sent : %s\n", msg.msg_text);
 
